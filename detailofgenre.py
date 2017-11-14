3353a0f0b0dd05d8dd679f0bee72bb9c47db6a47
Unknown <60070131@kmitl.ac.th>
2017-11-14 15:56:55 +0700

    add the most release game of 20 year
    แสดงผลเกมที่ปล่อยมากที่สุดใน20 ปี

--- detailofgenre.py
+++ detailofgenre.py
@@ -4,10 +4,12 @@ from pygal import *
 from pygal.style import * 
 import pandas as pd
 import matplotlib.pyplot as plt
+from pygal.style import NeonStyle
 games = pd.read_csv('C:/Users/Chalermwong/Desktop/ign.csv')
 genre = dict(games['genre'].value_counts())
 platform = dict(games['platform'].value_counts())
 release_year = dict(games['release_year'].value_counts())
+games.index = games['title']
 def Generation():
     pie_chart = pygal.Pie()
     pie_chart.title = 'Generation of games'
@@ -22,19 +24,19 @@ def Generation():
         print(e)
     pie_chart.render_to_file('bar_chart.svg')
 
-def bar_of_year(year):
+def genreofyear():
     #games['count'] = 0
     bar_chart = pygal.HorizontalBar()
     #fil = games.groupby(['release_year', 'genre']).count()
-    sports = games[(games.genre == 'Sports') & (games.release_year == year)]
-    action = games[(games.genre == 'Action') & (games.release_year == year)]
-    shooter = games[(games.genre == 'Shooter') & (games.release_year == year)]
-    racing = games[(games.genre == 'Racing') & (games.release_year == year)]
-    adventure = games[(games.genre == 'Adventure') & (games.release_year == year)]
-    strategy = games[(games.genre == 'Strategy') & (games.release_year == year)]
-    rpg = games[(games.genre == 'RPG') & (games.release_year == year)]
-    platformer = games[(games.genre == 'Platformer') & (games.release_year == year)]
-    puzzle = games[(games.genre == 'Puzzle') & (games.release_year == year)]
+    sports = games[(games.genre == 'Sports') & (games.release_year == 2012)]
+    action = games[(games.genre == 'Action') & (games.release_year == 2012)]
+    shooter = games[(games.genre == 'Shooter') & (games.release_year == 2012)]
+    racing = games[(games.genre == 'Racing') & (games.release_year == 2012)]
+    adventure = games[(games.genre == 'Adventure') & (games.release_year == 2012)]
+    strategy = games[(games.genre == 'Strategy') & (games.release_year == 2012)]
+    rpg = games[(games.genre == 'RPG') & (games.release_year == 2012)]
+    platformer = games[(games.genre == 'Platformer') & (games.release_year == 2012)]
+    puzzle = games[(games.genre == 'Puzzle') & (games.release_year == 2012)]
     #games.loc[games['release_year']== 2012] 
     sports = sports.loc[0:,['genre', 'release_year']]
     action = action.loc[0:,['genre', 'release_year']]
@@ -56,7 +58,7 @@ def bar_of_year(year):
     platformer = platformer.count()[0]
     puzzle = puzzle.count()[0]
 
-    bar_chart.title = 'the most genre of year '+str(year)
+    bar_chart.title = 'the most genre of 2012 '+str(2012)
     bar_chart.add('Sports', int(sports))
     bar_chart.add('Action', action)
     bar_chart.add('Shooter', shooter)
@@ -65,10 +67,30 @@ def bar_of_year(year):
     bar_chart.add('strategy', strategy)
     bar_chart.add('platformer', platformer)
     bar_chart.add('puzzle', puzzle)
-    bar_chart.render_to_file('yearofgame.svg')
+    bar_chart.render_to_file('genreofyear.svg')
     
 
     #bar_chart.render_to_file('bar_of_year.svg')
     #print(fil])
-    #print(sports)
-bar_of_year(int(input()))
+    #print(sports
+def yearofgame():
+    bar_chart = pygal.StackedLine(fill=True, interpolate='cubic', style=NeonStyle)
+    bar_chart.title  = 'The most games release'
+    get = SortedDisplayDict(release_year)
+    get = dict(sorted(get.items()))
+    bar_chart.x_labels = map(str, get.keys())
+    bar_chart.add('games', get.values())
+    bar_chart.render_to_file('yearofgame.svg')
+
+def splityear():
+    get = games['release_year'].unique()
+    print(get[1])
+
+
+
+
+class SortedDisplayDict(dict):
+   def __str__(self):  #sorted keys of dict
+       return "{" + ", ".join("%r: %r" % (key, self[key]) for key in sorted(self)) + "}"
+
+genreofyear()
